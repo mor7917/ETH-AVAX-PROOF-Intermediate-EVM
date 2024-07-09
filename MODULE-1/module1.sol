bcd8b73cfb64f231322b77c904cf1b8caf505d20
mor7917 <139258838+mor7917@users.noreply.github.com>
2024-07-09 23:27:16 +0530

    module1.sol

--- MODULE-1/module1.sol
+++ MODULE-1/module1.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
 
+/*
+Contract successfully uses require()
+Contract successfully uses assert()
+Contract successfully uses revert() statements
+*/
+pragma solidity ^0.8.20;
+
+contract functionanderror {
+    uint public age;
+    bool public isMale;
+    bool public isFeesPaid;
+
+    function HostelAdmission(uint256 _age, bool _isMale, bool _isFeesPaid) public {
+        require(_age > 18, "Age must be greater than 18");
+     
+        assert(_isMale == true);
+ 
+        if (_isFeesPaid == false) {
+            revert("Kindly Pay The Fees ");
+        }
+
+        age = _age;
+        isMale = _isMale;
+        isFeesPaid = _isFeesPaid;
+    }
