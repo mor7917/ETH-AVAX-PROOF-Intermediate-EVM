322e122adfa765aa67587d6bb7f393c7cda73556
mor7917 <139258838+mor7917@users.noreply.github.com>
2024-07-10 21:45:44 +0530

    module1.sol

--- MODULE-1/module1.sol
+++ MODULE-1/module1.sol
@@ -7,21 +7,22 @@ Contract successfully uses revert() statements
 */
 pragma solidity ^0.8.20;
 
-contract functionanderror {
+contract license {
     uint public age;
     bool public isMale;
-    bool public isFeesPaid;
+    bool public test;
 
-    function HostelAdmission(uint256 _age, bool _isMale, bool _isFeesPaid) public {
+    function LicenseTest(uint256 _age, bool _isMale, bool _test) public {
         require(_age > 18, "Age must be greater than 18");
      
         assert(_isMale == true);
  
-        if (_isFeesPaid == false) {
-            revert("Kindly Pay The Fees ");
+        if (_test == true) {
+            revert("You Passed the test ");
         }
 
         age = _age;
         isMale = _isMale;
-        isFeesPaid = _isFeesPaid;
+        test = _test;
     }
+}
